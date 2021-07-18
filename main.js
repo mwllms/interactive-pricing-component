078d427489b2bd7e6ed959eed7c6fcc6caad9e92
Mark Willems <mrkwillems@gmail.com>
2021-07-18 13:43:18 +0200

    Change background images to load in vite

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-import './style.css'
+import './style.css';
+import bgPattern from './assets/bg-pattern.svg';
+import circles from './assets/pattern-circles.svg';
 
-const slider = document.querySelector('input[type=range]')
-const pageviews = document.querySelector('[data-page-views]')
-const price = document.querySelector('[data-price]')
+const main = document.querySelector('main');
+const title = document.querySelector('[data-title]');
+const slider = document.querySelector('input[type=range]');
+const pageviews = document.querySelector('[data-page-views]');
+const price = document.querySelector('[data-price]');
 const pricing = [
   {
     views: '10K',
@@ -24,13 +28,16 @@ const pricing = [
     views: '1M',
     price: '36.00',
   },
-]
+];
+
+main.style.backgroundImage = `url(${bgPattern})`;
+title.style.backgroundImage = `url(${circles})`;
 
 slider.addEventListener('input', (e) => {
-  const val = pricing[e.target.value]
-  pageviews.innerHTML = val.views
-  price.innerHTML = val.price
-  
-  const progress = (e.target.value / 4) * 100
-  e.target.style.backgroundSize = `${progress}% 100%`
-})
+  const val = pricing[e.target.value];
+  pageviews.innerHTML = val.views;
+  price.innerHTML = val.price;
+
+  const progress = (e.target.value / 4) * 100;
+  e.target.style.backgroundSize = `${progress}% 100%`;
+});
